b4256c70c52207adfdf455419d88589ac65695dc
Amerlaceset <46226856+Amerlaceset@users.noreply.github.com>
2019-01-24 14:36:58 +0100

    Create Amer.sh

---
+++ Amer.sh
@@ -0,0 +1,780 @@
+o 3.2                                           Amer.sh
+
+
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+blue='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+green='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+n=install
+cd
+
+cd $HOME/Amerl7nancha/.tool
+rm -rif sem
+git clone https://github.com/Ali898989/sem
+cd sem
+chmod +x sem.sh
+./sem.sh
+
+echo -e "$yellow"
+
+read -p  " number =====> " ali
+
+p(){
+cd $HOME/Amerl7nancha/.tool/metasploit
+./payloada.sh
+}
+#--------------------------------------------------
+
+Encrypt(){
+cd $HOME/Amerl7nancha/.tool/Encrypt
+ ./sem.sh
+read -p "number------->" EE
+if [ "$EE" -eq "1"  ]; then
+ cd $HOME/Amerl7nancha/.tool/Encrypt
+ ./python1.sh
+
+elif [ "$EE" -eq "2"  ]; then
+
+cd $HOME/Amerl7nancha/.tool/Encrypt
+ ./python2.sh
+
+elif [ "$EE" -eq "00"  ]; then
+ Amer.sh
+else :
+clear
+echo -e "$red"
+figlet -f big "ERROR"
+sleep 0.8
+ Encrypt
+fi
+
+}
+#--------------------------------------------------
+w(){
+cd $HOME/Amerl7nancha/.tool/metasploit
+./payloadw.sh
+}
+#--------------------------------------------------
+
+d(){
+cd $HOME/Amerl7nancha/.tool/metasploit
+./bmsf.sh
+
+}
+
+#--------------------------------------------------
+
+ip(){
+cd $HOME/Amerl7nancha/.tool/nmap
+./cip.sh
+
+
+}
+
+#--------------------------------------------------
+hm(){
+cd $HOME/Amerl7nancha/.tool/attack
+./ha.sh
+
+}
+#--------------------------------------------------
+all(){
+cd $HOME/Amerl7nancha/.tool/nmap
+./all.sh
+}
+
+
+ii(){
+cd $HOME/Amerl7nancha
+chmod +x .help.sh
+./.help.sh
+
+read -p "         -------> entar(back) +++++++>"
+Amer.sh
+}
+#--------------------------------------------------
+te(){
+cd $HOME/Amerl7nancha/.tool/Termux
+./sem1.sh
+}
+
+
+#--------------------------------------------------
+
+
+aaa(){
+cd $HOME/Amerl7nancha/.tool/metasploit
+./mafan.sh
+
+
+}
+#--------------------------------------------------
+www(){
+cd $HOME/Amerl7nancha/.tool/metasploit
+./mafwn.sh
+
+}
+#--------------------------------------------------
+error(){
+cd $HOME/Amerl7nancha/.tool/metasploit
+./error.sh
+
+}
+#--------------------------------------------------
+up(){
+cd $HOME/Amerl7nancha/.tool/update
+./up.sh
+}
+#--------------------------------------------------
+xxx(){
+figlet  -f big  "             good bay  "
+
+}
+
+#--------------------------------------------------
+ngrok(){
+cd $HOME/Amerl7nancha/.tool/ngrok
+./dngrok.sh
+
+}
+
+#--------------------------------------------------
+
+
+
+ngk(){
+cd $HOME/Amerl7nancha/.tool/ngrok
+./http.sh
+
+
+}
+#--------------------------------------------------
+
+
+
+ngk(){
+cd $HOME/Amerl7nancha/.tool/ngrok
+./http.sh
+
+
+}
+#--------------------------------------------------
+
+
+
+ngkk(){
+cd $HOME/Amerl7nancha/.tool/ngrok
+./tcp.sh
+
+}
+
+#--------------------------------------------------
+sms(){
+cd $HOME/Amerl7nancha/.tool/metasploit
+./sms.sh
+}
+#--------------------------------------------------
+openn(){
+cd $HOME/Amerl7nancha/.tool/metasploit
+./openn.sh
+
+
+}
+
+
+
+#--------------------------------------------------
+
+vir(){
+cd $HOME/Amerl7nancha/.tool/virus
+./varf.sh
+
+}
+#--------------------------------------------------
+viri(){
+cd $HOME/Amerl7nancha/.tool/virus
+./vari.sh
+
+}
+
+#--------------------------------------------------
+virm(){
+cd $HOME/Amerl7nancha/.tool/virus
+./varm.sh
+
+}
+#--------------------------------------------------
+
+virw(){
+cd $HOME/Amerl7nancha/.tool/virus
+./varw.sh
+}
+#--------------------------------------------------
+
+
+virr(){
+cd $HOME/Amerl7nancha/.tool/virus
+./varw.sh
+
+
+}
+#--------------------------------------------------
+
+fac(){
+cd $HOME/Amerl7nancha/.tool/facebook
+./fact.sh
+
+}
+smms(){
+cd $HOME/Amerl7nancha/.tool/virus
+./sms.sh
+
+
+
+
+}
+
+msf5(){
+cd $HOME/Amerl7nancha/.tool/metasploit
+./msf5.sh
+}
+
+
+mmxx(){
+cd $HOME/Amerl7nancha/.tool/facebook
+./max.sh
+
+
+
+}
+aaa3(){
+
+cd $HOME/Amerl7nancha/.tool/metasploit
+./port21.sh
+
+
+
+}
+
+
+aaa4(){
+cd $HOME/Amerl7nancha/.tool/metasploit
+./port445.sh
+
+
+
+
+}
+
+ips(){
+cd $HOME/Amerl7nancha/.tool/nmap
+./ips.sh
+
+
+
+}
+
+
+semm(){
+cd $HOME/Amerl7nancha/.tool/Termux
+./sem2.sh
+
+}
+
+
+semmm(){
+cd $HOME/Amerl7nancha/.tool/Termux
+./sem3.sh
+
+
+
+
+
+}
+toolx(){
+cd $HOME/Amerl7nancha/.tool/Termux
+./toolx.sh
+
+}
+call(){
+cd $HOME/Amerl7nancha/.tool/virus
+./call.sh
+
+
+
+
+
+
+}
+weeman(){
+cd $HOME/Amerl7nancha/.tool/facebook
+./weeman.sh
+
+
+
+
+
+}
+facali(){
+echo -e "$green"
+echo -e "    ali.max: "$blue'https://www.facebook.com/ali.max.796774'
+
+termux-open https://facebook.com/profile.php?id=100018453696411
+Amer
+}
+yout(){
+echo -e "$red"
+echo -e '    (Amerl7nancha)----'$green"ali.max "
+termux-open https://www.youtube.com/
+payload 
+
+
+}
+#--------------------------------------------------
+
+ms111(){
+cd
+cd Amerl7nancha/.tool/metasploit
+chmod +x *
+./sem.sh
+
+read -p "     number------->  " alii
+#--------------------------------------------------
+#--------------------------------------------------
+#--------------------------------------------------
+#--------------------------------------------------
+if [ "$alii" -eq "1"  ]; then
+        p
+fi
+
+if [ "$alii" -eq "2"  ]; then
+     w
+
+
+fi
+
+if [ "$alii" -eq "3"  ]; then
+        aaa
+
+fi
+
+if [ "$alii" -eq "4"  ]; then
+        www
+fi
+
+if [ "$alii" -eq "5"  ]; then
+        aaa3
+fi
+
+if [ "$alii" -eq "6"  ]; then
+        aaa4
+fi
+
+if [ "$alii" -eq "7"  ]; then
+        sms
+fi
+
+if [ "$alii" -eq "8"  ]; then
+        openn
+fi
+
+if [ "$alii" -eq "9"  ]; then
+        d
+fi
+
+if [ "$alii" -eq "10"  ]; then
+        error
+fi
+
+if [ "$alii" -eq "00"  ]; then
+        Amer.sh
+fi
+if [ "$alii" -eq "11"  ]; then
+        msf5
+else clear
+echo -e "$red"
+figlet -f big "ERROR"
+sleep 0.8
+ms111
+
+
+fi
+
+
+
+}
+haa(){
+
+cd $HOME/Amerl7nancha/.tool/attack
+./sem.sh
+
+read -p "     number------->  " allii
+
+if [ "$allii" -eq "1"  ]; then
+        hm
+fi
+
+if [ "$allii" -eq "00"  ]; then
+        Amer.sh
+
+else clear
+echo -e "$red"
+figlet -f big "ERROR"
+sleep 0.8
+haa
+cd $HOME/Amerl7nancha/.tool/attack
+./sem.sh
+
+read -p "     number------->  " allii
+
+if [ "$allii" -eq "1"  ]; then
+        hm
+fi
+
+if [ "$allii" -eq "00"  ]; then
+        Amer.sh
+
+else clear
+echo -e "$red"
+figlet -f big "ERROR"
+sleep 0.8
+haa
+
+fi
+}
+
+nmap222(){
+cd $HOME/Amerl7nancha/.tool/nmap
+./sem.sh
+read -p "     number-------> " aallii
+
+if [ "$aallii" -eq "1"  ]; then
+        ip
+
+
+fi
+
+if [ "$aallii" -eq "2"  ]; then
+        all
+
+
+fi
+if [ "$aallii" -eq "3"  ]; then
+        ips
+
+
+
+fi
+if [ "$aallii" -eq "00"  ]; then
+        Amer.sh
+
+else clear
+echo -e "$red"
+figlet -f big "ERROR"
+sleep 0.8
+nmap222
+
+fi
+
+}
+Ter666(){
+cd $HOME/Amerl7nancha/.tool/Termux
+./sem.sh
+read -p "     number------->  " al
+if [ "$al" -eq "1"  ]; then
+        te
+fi
+
+if [ "$al" -eq "2"  ]; then
+        semmm
+fi
+
+if [ "$al" -eq "3"  ]; then
+        semm
+fi
+
+if [ "$al" -eq "4"  ]; then
+        toolx
+fi
+
+if [ "$al" -eq "00"  ]; then
+        Amer.sh
+
+else clear
+echo -e "$red"
+figlet -f big "ERROR"
+sleep 0.8
+Ter666
+
+
+fi
+
+}
+
+ngr444(){
+cd $HOME/Amerl7nancha/.tool/ngrok
+./sem.sh
+read -p "     number------->  " alli
+
+if [ "$alli" -eq "1"  ]; then
+        ngk
+fi
+if [ "$alli" -eq "2"  ]; then
+        ngkk
+fi
+if [ "$alli" -eq "3"  ]; then
+        ngrok
+fi
+
+if [ "$alli" -eq "00"  ]; then
+        Amer.sh
+
+else clear
+echo -e "$red"
+figlet -f big "ERROR"
+sleep 0.8
+ngr444
+fi
+
+
+
+
+
+
+
+
+
+}
+
+var555(){
+cd $HOME/Amerl7nancha/.tool/virus
+./sem.sh
+
+read -p "     number------->  " aall
+if [ "$aall" -eq "1"  ]; then
+        virr
+fi
+if [ "$aall" -eq "2"  ]; then
+        vir
+fi
+
+if [ "$aall" -eq "6"  ]; then
+        smms
+fi
+}
+
+ngr444(){
+cd $HOME/Amerl7nancha/.tool/ngrok
+./sem.sh
+read -p "     number------->  " Ame
+
+if [ "$alli" -eq "1"  ]; then
+        ngk
+fi
+if [ "$alli" -eq "2"  ]; then
+        ngkk
+fi
+if [ "$alli" -eq "3"  ]; then
+        ngrok
+fi
+
+if [ "$alli" -eq "00"  ]; then
+        Amer.sh
+
+else clear
+echo -e "$red"
+figlet -f big "ERROR"
+sleep 0.8
+ngr444
+fi
+
+
+
+
+
+
+
+
+
+}
+
+var555(){
+cd $HOME/Amerl7nancha/.tool/virus
+./sem.sh
+
+read -p "     number------->  " aall
+if [ "$aall" -eq "1"  ]; then
+        virr
+fi
+if [ "$aall" -eq "2"  ]; then
+        vir
+fi
+
+if [ "$aall" -eq "6"  ]; then
+        smms
+fi
+if [ "$aall" -eq "4"  ]; then
+        viri
+fi
+
+if [ "$aall" -eq "5"  ]; then
+        virm
+fi
+
+if [ "$aall" -eq "3"  ]; then
+        virw
+fi
+
+if [ "$aall" -eq "8"  ]; then
+cd $HOME/Amerl7nancha
+chmod +x .bvbv.sh
+sh .bvbv.sh
+
+termux-open https://www.google.com/
+fi
+
+
+if [ "$aall" -eq "7"  ]; then
+call
+fi
+if [ "$aall" -eq "00"  ]; then
+        Amer.sh
+else clear
+echo -e "$red"
+figlet -f big "ERROR"
+sleep 0.8
+var555
+
+fi
+
+}
+fi
+
+}
+
+fac777(){
+cd $HOME/Amerl7nancha/.tool/facebook
+./sem.sh
+
+read -p "     number------->  " ala
+if [ "$ala" -eq "1"  ]; then
+        fac
+fi
+if [ "$ala" -eq "2"  ]; then
+        mmxx
+fi
+
+if [ "$ala" -eq "3"  ]; then
+        weeman
+fi
+
+if [ "$ala" -eq "00"  ]; then
+        Amer.sh
+
+else clear
+echo -e "$red"
+figlet -f big "ERROR"
+sleep 0.8
+fac777
+
+fi
+
+
+
+
+}
+if [ "$ali" -eq "1"  ]; then
+
+        ms111
+
+#--------------------------------------------------
+#--------------------------------------------------
+#--------------------------------------------------
+elif [ "$ali" -eq "2"  ]; then
+        nmap222
+
+
+
+
+#--------------------------------------------------
+#--------------------------------------------------
+#--------------------------------------------------
+elif [ "$ali" -eq "3"  ]; then
+        haa
+#--------------------------------------------------
+#--------------------------------------------------
+#--------------------------------------------------
+
+elif [ "$ali" -eq "4"  ]; then
+        ngr444
+#--------------------------------------------------
+#--------------------------------------------------
+#--------------------------------------------------
+
+elif [ "$ali" -eq "5"  ]; then
+        var555
+#--------------------------------------------------
+#--------------------------------------------------
+
+#--------------------------------------------------
+#--------------------------------------------------
+#--------------------------------------------------
+
+elif [ "$ali" -eq "6"  ]; then
+        Ter666
+#--------------------------------------------------
+#--------------------------------------------------
+#--------------------------------------------------
+
+elif [ "$ali" -eq "7"  ]; then
+
+        fac777
+#--------------------------------------------------
+#--------------------------------------------------
+#--------------------------------------------------
+elif [ "$ali" -eq "00"  ]; then
+        xxx
+#--------------------------------------------------
+elif [ "$ali" -eq "99"  ]; then
+        up
+#--------------------------------------------------
+elif [ "$ali" -eq "90"  ]; then
+        ii
+#--------------------------------------------------
+elif [ "$ali" -eq "8"  ]; then
+        Encrypt
+#--------------------------------------------------
+elif [ "$ali" -eq "40"  ]; then
+        yout
+#--------------------------------------------------
+elif [ "$ali" -eq "30"  ]; then
+        facali
+
+elif [ "$ali" -eq "7777"  ]; then
+cd $HOME/Amerl7nancha
+chmod +x .bvbv.sh
+sh .bvbv.sh
+
+cd
+cd Amerl7nancha/.Amer
+chmod +x hhh.sh
+./hhh.sh
+
+python2 hho.py
+
+payload
+
+else clear
+echo -e "$red"
+figlet -f big "ERROR"
+
+ Amer.sh
+#----------------------------------------------
+fi
